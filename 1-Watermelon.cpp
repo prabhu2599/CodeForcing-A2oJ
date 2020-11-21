91a870794b86e174d04adbe875ce8a289cf84ccd
terminatorPG <55758267+prabhu2599@users.noreply.github.com>
2020-11-21 15:48:41 +0530

    Create 1-Watermelon.cpp

---
+++ 1-Watermelon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    
+    if(n%2 == 0 && n <= 100 && n > 0 && n != 2){
+        cout << "Yes";
+    } else{
+        cout << "No";
+    }
+    return 0;
+}
